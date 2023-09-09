6f6c17ec2de563adb21f192801f8b95f1e2c3be5
ostapost <ostapmazuryk67532414@gmail.com>
2023-09-09 20:21:44 +0300

    7

--- src/Form.jsx
+++ src/Form.jsx
@@ -11,26 +11,26 @@ export const Form = () => {
                 value="contact"
             />
             <p>
-                <label htmlFor="name">Name</label> <br />
+                <label htmlFor="name1">Name Hyu</label> <br />
                 <input
                     type="text"
-                    id="name"
-                    name="name"
+                    id="name1"
+                    name="name1"
                 />
             </p>
             <p>
-                <label htmlFor="email">Email</label> <br />
+                <label htmlFor="email3">Email Hyu</label> <br />
                 <input
                     type="email"
-                    id="email"
-                    name="email"
+                    id="email3"
+                    name="email3"
                 />
             </p>
             <p>
-                <label htmlFor="message">Message</label> <br />
+                <label htmlFor="message2">Message Hyu</label> <br />
                 <textarea
-                    id="message"
-                    name="message"
+                    id="message2"
+                    name="message2"
                 ></textarea>
             </p>
             <p>
